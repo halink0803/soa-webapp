e599e17b75843d4e8d60a0d47697db3e447d0146
halink0803 <halink0803@gmail.com>
2015-10-04 23:36:21 +0700

    update webapp view

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 var bookList = 	angular.module('bookList', []);
 
 bookList.controller('bookListController', function($scope, $http) {
+    $scope.bookList = {
+        books :[{
+          name: 'Service Oriented Architecture',
+          year: '2015',
+          field: 'programming',
+          author: 'Ha Link',
+          code: '2012',
+          publisher: 'UET',
+          image: '',
+          pages: '2015'
+        }]
+      }
 	delete $http.defaults.headers.common['X-Requested-With'];
 	$http.get("http://restful-soa.esy.es/listCategories")
     .success(function(response) {    	
@@ -12,7 +24,8 @@ bookList.controller('bookListController', function($scope, $http) {
     .success(function(response) {
     	jQuery('.category .mdl-spinner').css('display', 'none');
     	$scope.bookList = response;
-    });    
+    });
+    $scope.book_detail = $scope.bookList.books[0];    
     $scope.category_click = function( $event ) {
     							jQuery('.category .active').removeClass('active');
     							if( !jQuery($event.currentTarget).hasClass('active') ){
@@ -29,6 +42,17 @@ bookList.controller('bookListController', function($scope, $http) {
     									jQuery(this).parent().parent().parent().css('display', 'block');
     								}
     							})
-    						}   
+    						};
+    $scope.readmore_click = function ($event) {
+                          var index = jQuery($event.currentTarget).attr('index');    
+                          $scope.book_detail = $scope.bookList.books[index];
+                          console.log( $scope.book_detail );
+                          jQuery('.book_detail_section').addClass('active');
+                          // ipc.send('show-book-detail');
+                        };
+    $scope.close = function($event) {
+                          $event.preventDefault();
+                          jQuery('.book_detail_section').removeClass('active');
+                      }                                                       
 })
 
